d283cf13d9a813b2cff86b63b8e0f8615e23d50b
Hao Ren <renh.cn@gmail.com>
2016-02-26 17:20:19 +0800

    add outcar.py for OUTCAR and corresponding contents in pietas.py, add __future__ import for grid.py

---
+++ outcar.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import numpy as np
+
+class OUTCAR:
+    """
+    OUTCAR class for VASP OUTCAR files.
+    """
+    def __init__(fname = 'OUTCAR'):
+        pass
+    
