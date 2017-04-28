8aa005980e3944ff1a785e8a292286906eb1fab1
Hao Ren <ren@chimy.upc.edu.cn>
2017-04-28 17:47:49 +0800

    Test module for wavefunction overlaps. Aug chg

---
+++ overlap.py
@@ -0,0 +1,82 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2017 Hao Ren <renh.cn@gmail.com>
+#
+# Distributed under terms of the LGPLv3 license.
+
+"""
+overlap calculation for PAW AE wavefunction verification.
+"""
+# Module import
+from __future__ import print_function
+import numpy as np
+import wavefunction as wf
+import normalcar as nm
+#==============================================================================
+
+
+def calc_aug_charge(bm, bn, LMMax, Qij, Pij):
+    NTypes = len(LMMax)
+    augchg = 0
+    ion_ind = 0
+    proj_ind = 0
+    for itype in range(NTypes):
+        NChannels, NIons = LMMax[itype]
+        for iion in range(NIons):
+            qij = Qij[:LMMax[itype,0], :LMMax[itype,0], ion_ind, 0]
+            pi = Pij[proj_ind:proj_ind+LMMax[itype,0], bm, 0, 0]
+            pj = Pij[proj_ind:proj_ind+LMMax[itype,0], bn, 0, 0]
+            
+            augchg += np.dot(np.conj(pi), np.dot(qij, pj))
+            
+            ion_ind += 1
+            proj_ind += LMMax[itype, 0]
+    return augchg
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    wc = wf.WAVECAR('../test/WAVECAR')
+    nmc = nm.NormalCAR(normalcar='../test/NormalCAR', 
+            wavecar='../test/WAVECAR')
+    
+    # read pseudo wave function
+    wps = wf.WaveFunction(wc, ispin=0, ik=0)
+    psi_ps = wps.getWPS()
+    print(psi_ps.shape)
+    NBands = len(psi_ps)
+
+    # read projector info
+    LMDIM = nmc.getLMDim()
+    Qij = nmc.getCQij()
+    Pij = nmc.getProjCoeffs()
+    LMMax = nmc.getLMMax()
+    print(Qij.shape)
+    print(Pij.shape)
+    print(LMMax)
+
+    # overlap between WPS
+    S_PS = np.dot(
+        np.conj(psi_ps), psi_ps.T 
+            )
+    print(S_PS.shape)
+
+    
+    # augmented charge
+    S = np.zeros([NBands, NBands], dtype=np.complex128)
+    for bm in range(NBands):
+        for bn in range(bm, NBands):
+            S[bm, bn] = S_PS[bm, bn] + \
+                    calc_aug_charge(bm, bn, LMMax, Qij, Pij)
+            if (bm != bn):
+                S[bn, bm] = S[bm, bn]
+
+    np.save('S.npy', S)
+
+
